07e889f5778d26d9b87ff31e3ef74cc51840ff73
dorarad17 <dorarad17@gmail.com>
2020-02-08 15:08:26 +0000

    changed json to be array of tasks

--- handler.js
+++ handler.js
@@ -5,7 +5,34 @@ const app = express();
 
 app.get("/tasks", function(req, res) {
 	res.json({
-		message: "Hello World!"
+		tasks: [
+			{ id: 1, description: "Get milk", completed: false, priority: 1 },
+			{
+				id: 2,
+				description: "Clean kitchen",
+				completed: false,
+				priority: 2
+			},
+			{
+				id: 3,
+				description: "Cook dinner",
+				completed: false,
+				priority: 3
+			},
+
+			{
+				id: 4,
+				description: "Other task",
+				completed: false,
+				priority: 3
+			},
+			{
+				id: 5,
+				description: "More tasks",
+				completed: false,
+				priority: 3
+			}
+		]
 	});
 });
 
