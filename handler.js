6cec5abcb51950e7a0a2763574646e672cb60e32
dorarad17 <dorarad17@gmail.com>
2020-02-08 15:38:11 +0000

    fixed uuid bug with npm package and different invoke

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 "use strict";
+const uuidv4 = require("uuid/v4");
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
@@ -6,28 +7,28 @@ const app = express();
 app.get("/tasks", function(req, res) {
 	res.json({
 		tasks: [
-			{ id: 1, description: "Get milk", completed: false, priority: 1 },
+			{ id: uuidv4(), description: "Get milk", completed: false, priority: 1 },
 			{
-				id: 2,
+				id: uuidv4(),
 				description: "Clean kitchen",
 				completed: false,
 				priority: 2
 			},
 			{
-				id: 3,
+				id: uuidv4(),
 				description: "Cook dinner",
 				completed: false,
 				priority: 3
 			},
 
 			{
-				id: 4,
+				id: uuidv4(),
 				description: "Other task",
 				completed: false,
 				priority: 3
 			},
 			{
-				id: 5,
+				id: uuidv4(),
 				description: "More tasks",
 				completed: false,
 				priority: 3
