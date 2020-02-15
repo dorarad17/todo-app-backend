d25bdcd326daf4ef8a65abea9c12fdc3268d38eb
dorarad17 <dorarad17@gmail.com>
2020-02-15 14:15:48 +0000

    connection with my sql to fetch tasks from data base working

--- handler.js
+++ handler.js
@@ -6,43 +6,27 @@ const app = express();
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-	host: "example.orgtr-course-rds.cettgt1wpnod.eu-west-2.rds.amazonaws.com",
+	host: "xxx",
 	user: "xxxx",
-	password: "xxxxx",
-	database: "xxxx"
+	password: "xxxx",
+	database: "xxxxx"
 });
 
 // Retrieving tasks
 app.get("/tasks", function(req, res) {
-	res.json({
-		tasks: [
-			{ id: uuidv4(), description: "Get milk", completed: false, priority: 1 },
-			{
-				id: uuidv4(),
-				description: "Clean kitchen",
-				completed: false,
-				priority: 2
-			},
-			{
-				id: uuidv4(),
-				description: "Cook dinner",
-				completed: false,
-				priority: 3
-			},
-
-			{
-				id: uuidv4(),
-				description: "Other task",
-				completed: false,
-				priority: 3
-			},
-			{
-				id: uuidv4(),
-				description: "More tasks",
-				completed: false,
-				priority: 3
-			}
-		]
+	connection.query('SELECT * FROM `tasks` WHERE `userId` = "364"', function(
+		error,
+		results,
+		fields
+	) {
+		// error will be an Error if one occurred during the query
+		if (error) {
+			console.error("error fetching tasks", error);
+			res.status(500).json({ errorMessage: error });
+		} else {
+			// Query was successful
+			res.json({ tasks: results });
+		}
 	});
 });
 
