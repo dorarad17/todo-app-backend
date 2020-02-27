2c54faa9513c717ab87c15ad506cafb928e2140f
dorarad17 <dorarad17@gmail.com>
2020-02-27 20:42:10 +0000

    installed and added cors

--- handler.js
+++ handler.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const mysql = require("mysql");
+const cors = require("cors");
+app.use(cors());
 
 const connection = mysql.createConnection({
 	host: process.env.DB_HOST,
